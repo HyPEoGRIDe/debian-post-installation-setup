264b4ca37175c2a27f7bddf53465f7974e59590a
k <k@debian.debian>
2021-03-29 01:32:54 +0400

    new changes

--- post-install.sh
+++ post-install.sh
@@ -8,10 +8,13 @@ sudo aptitude autoclean
 
 cp ~/.bashrc ~/.bashrc_backup
 
-echo "alias pkgin='sudo aptitude install'"
-echo "alias pkgpu='sudo aptitude purge'"
-echo "alias pkgupd='sudo aptitude update'"
-echo "alias pkgse='aptitude search'"
-echo "alias sysupg='sudo aptitude update && sudo aptitude full-upgrade'"
+echo "#--------------------CUSTOM ALIASES--------------------" >> ~/.bashrc
+echo "alias pkgin='sudo aptitude install'" >> ~/.bashrc
+echo "alias pkgpu='sudo aptitude purge'" >> ~/.bashrc
+echo "alias pkgupd='sudo aptitude update'" >> ~/.bashrc
+echo "alias pkgse='aptitude search'" >> ~/.bashrc
+echo "alias sysupg='sudo aptitude update && sudo aptitude full-upgrade'" >> ~/.bashrc
+
+#test 1
 
 sudo aptitude install neofetch htop git
